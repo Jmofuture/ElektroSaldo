1b7add890cd72026170bfd48c5a7e29fdd86ad65
Jean Olmedillo <jmof.html@gmail.com>
2022-01-04 23:53:32 -0300

    resuelto la carga inicial

--- script/gamesDom.js
+++ script/gamesDom.js
@@ -1,88 +1,109 @@
 $(document).ready(function () {
-  displayList(); //Al cargar la pagina llamamos  ala funcion que genera las cards
+   //Al cargar la pagina llamamos  a la funcion que genera las cards
   if (localStorage.getItem("favsList")) {
     favList = JSON.parse(localStorage.getItem("favsList")); //Verificamos si en el LS hay Data para dibujar las cards del listado
     printfav();
   }
+
+  setUrl()//Inicializamos la funcion que asigna la URL de la API por defecto o al cambiar de option
 });
 
-//Funcion que lla a la API y luego recorre los elementos para dibujar las cards en el DOM
 
-function displayList() {
-  const selectGenre = document.getElementById("typeGame");
-  let urlList = selectGenre.addEventListener("change", (e) => {
-    let option = e.target.value;
-    let GETURL =  option;
-    console.log(GETURL)
-    $.get(GETURL).done(function (result, state) {
-      //Usamos el metodo GET de JQ
-      if (state === "success") {
-        let games = result; //Asignamos el resultado a una variable que es la que tiene todo los datos
-        games.forEach((gameCard) => {
-          let cardContainer = document.createElement("div");
-          cardContainer.className = "card";
-          mainContainer.appendChild(cardContainer);
+//Funcion que captura el evento de seleccion 
+let selectGenre = document.getElementById("typeGame").addEventListener("change", (e) => {
+  let option = e.target.value;
+  setUrl(option)
+})
 
-          let image = document.createElement("img");
-          image.src = `${gameCard.thumbnail}`;
-          image.setAttribute("alt", `${gameCard.short_description}`);
-          cardContainer.appendChild(image);
 
-          let title = document.createElement("h5");
-          title.className = "nameTitle";
-          title.textContent = `${gameCard.title}`;
-          cardContainer.appendChild(title);
+//Funcion que recibe el evento y asigna a una variable la opcion seleccionada y a su vez asigna la url por defecto al cargar la pagina
+function setUrl(opt) {
+  const optionUrls = {
+    Anime:  "https://www.freetogame.com/api/games?category=Anime",
+    "battle-royale": "https://www.freetogame.com/api/games?category=battle-royale",
+    sports: "https://www.freetogame.com/api/games?category=sports",
+    fantasy: "https://www.freetogame.com/api/games?category=fantasy",
+    "open-world": "https://www.freetogame.com/api/games?category=open-world",
+    fighting: "https://www.freetogame.com/api/games?category=fighting",
+    shooter: "https://www.freetogame.com/api/games?category=shooter",
+    games: "https://www.freetogame.com/api/games"
+  }
+  
+  const urlDefault = "https://www.freetogame.com/api/games?category=Anime"
+selectUrl = optionUrls[opt];
+console.log(selectUrl, urlDefault)
 
-          let console = document.createElement("h6");
-          console.className = "platform";
-          console.textContent = `${gameCard.platform}`;
-          cardContainer.appendChild(console);
+displayList(selectUrl, urlDefault)
+}
 
-          let genre = document.createElement("p");
-          genre.className = "genre";
-          genre.textContent = `${gameCard.genre}`;
-          cardContainer.appendChild(genre);
 
-          let anchor = document.createElement("a");
-          anchor.textContent = "Enlace";
-          anchor.href = `${gameCard.freetogame_profile_url}`;
-          anchor.setAttribute("target", "_blank");
-          cardContainer.appendChild(anchor);
+//Funcion que llama la API con la URl por defecto o la opcion y luego recorre los elementos para dibujar las cards en el DOM
+function displayList(url1 ,url2) {
+  $.get(url1 || url2).done(function (result, state) {
+    //Usamos el metodo GET de JQ
+    if (state === "success") {
+      let games = result; //Asignamos el resultado a una variable que es la que tiene todo los datos
+      console.log(games)
+      let mainContainer = document.getElementById("mainContainer");
+      mainContainer.textContent=""
+      games.forEach((gameCard) => {
 
-          let btnAddGame = document.createElement("button");
-          btnAddGame.className = "button btnAddToList";
-          btnAddGame.id = `${gameCard.id}`;
-          btnAddGame.textContent = "Favorito";
-          cardContainer.appendChild(btnAddGame);
+        let cardContainer = document.createElement("div");
+        cardContainer.className = "card";
+        mainContainer.appendChild(cardContainer);
 
-          //Funcion que escucha el click de Agregar, busca cual es el elemento mediuante el ID
-          document.getElementById(`${gameCard.id}`).addEventListener("click", (e) => {
-            Swal.fire({
-              icon: "success",
-              title: `${gameCard.title}`,
-              text: "Agregado a Favoritos",
-              toast: true,
-              position: "bottom-end",
-              showConfirmButton: false,
-              timer: 1500,
-              background: "rgba(99,31,235,0.8)",
-              color: "#ffffff",
-            });
-            //Ejecutamos la funcion que busca en el LS si existe o no (Se encuentra en el otro documento JS)
-            getList(games.find((g) => g.id == `${gameCard.id}`));
+        let image = document.createElement("img");
+        image.src = `${gameCard.thumbnail}`;
+        image.setAttribute("alt", `${gameCard.short_description}`);
+        cardContainer.appendChild(image);
 
-            //   if (getList = undefined) {
+        let title = document.createElement("h5");
+        title.className = "nameTitle";
+        title.textContent = `${gameCard.title}`;
+        cardContainer.appendChild(title);
 
-            //       icon = document.createElement("i")
-            //       icon.className = "far fa-bookmark";
-            //       title.appendChild(icon)
+        let console = document.createElement("h6");
+        console.className = "platform";
+        console.textContent = `${gameCard.platform}`;
+        cardContainer.appendChild(console);
 
-            //     }
-            printfav(e);
+        let genre = document.createElement("p");
+        genre.className = "genre";
+        genre.textContent = `${gameCard.genre}`;
+        cardContainer.appendChild(genre);
+
+        let anchor = document.createElement("a");
+        anchor.textContent = "Enlace";
+        anchor.href = `${gameCard.freetogame_profile_url}`;
+        anchor.setAttribute("target", "_blank");
+        cardContainer.appendChild(anchor);
+
+        let btnAddGame = document.createElement("button");
+        btnAddGame.className = "button btnAddToList";
+        btnAddGame.id = `${gameCard.id}`;
+        btnAddGame.textContent = "Favorito";
+        cardContainer.appendChild(btnAddGame);
+
+        //Funcion que escucha el click de Agregar, busca cual es el elemento mediuante el ID
+        document.getElementById(`${gameCard.id}`).addEventListener("click", (e) => {
+          Swal.fire({
+            icon: "success",
+            title: `${gameCard.title}`,
+            text: "Agregado a Favoritos",
+            toast: true,
+            position: "bottom-end",
+            showConfirmButton: false,
+            timer: 1500,
+            background: "rgba(99,31,235,0.8)",
+            color: "#ffffff",
           });
+          //Ejecutamos la funcion que busca en el LS si existe o no (Se encuentra en el otro documento JS)
+          getList(games.find((g) => g.id == `${gameCard.id}`));
+
+          printfav(e);
         });
-      }
-    });
+      });
+    }
   });
 }
 
@@ -101,7 +122,9 @@ btnCloseList.addEventListener("click", () => {
 
 //FUNCION PARA VALIDAR EL FORMULARIO Y USAR EL METODO POST
 let btnSend = document.getElementById('sendForm').addEventListener('click', async () => {
-  const { value: email } = await Swal.fire({ //SweetAlert que pide direccion email
+  const {
+    value: email
+  } = await Swal.fire({ //SweetAlert que pide direccion email
     title: 'Dirección de email',
     input: 'email',
     inputPlaceholder: 'johnDoe@email.com',
@@ -115,28 +138,28 @@ let btnSend = document.getElementById('sendForm').addEventListener('click', asyn
   })
   if (email) {
     await Swal.fire({
-      title : `Email ingresado: ${email}`,
-      background: "rgba(99,31,235,0.8)",
-      color: "#ffffff",
-      confirmButtonColor: 'rgb(223, 91, 15)',
-    }), 
-      fetch('https://jsonplaceholder.typicode.com/posts', { //Metodo Post de JSONPlaceHolder
-      method: 'POST',
-      body: JSON.stringify({
-        title: 'email',
-        body: `${email}`,
-        userId: 1,
+        title: `Email ingresado: ${email}`,
+        background: "rgba(99,31,235,0.8)",
+        color: "#ffffff",
+        confirmButtonColor: 'rgb(223, 91, 15)',
       }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
+      fetch('https://jsonplaceholder.typicode.com/posts', { //Metodo Post de JSONPlaceHolder
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'email',
+          body: `${email}`,
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
       .then((response) => response.json())
       .then((json) => console.log(json));
   }
 })
 
-//Variable que da la fecha de la pagina en el footer
+//Variable que da la fecha a la pagina en el footer
 
 let dateFooter = document.getElementById('datePag')
 dateFooter.textContent = new Date().getFullYear();
